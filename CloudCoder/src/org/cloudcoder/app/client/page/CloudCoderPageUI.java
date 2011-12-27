5105200a4f524025796ec3c4b95413932ec83f75
David Hovemeyer <david.hovemeyer@gmail.com>
2011-12-27 16:55:38 -0500

    inlined remaining *PageUI classes into the corresponding *Page classes
    eliminated CloudCoderPageUI interface

--- CloudCoder/src/org/cloudcoder/app/client/page/CloudCoderPageUI.java
+++
@@ -1,8 +0,0 @@
-package org.cloudcoder.app.client.page;
-
-
-import com.google.gwt.user.client.ui.IsWidget;
-
-public interface CloudCoderPageUI extends IsWidget, SessionObserver {
-	public void setPage(CloudCoderPage page);
-}
--- CloudCoder/src/org/cloudcoder/app/client/page/LoginPage.java
+++ CloudCoder/src/org/cloudcoder/app/client/page/LoginPage.java
@@ -1,13 +1,178 @@
 package org.cloudcoder.app.client.page;
 
+import org.cloudcoder.app.client.model.Session;
+import org.cloudcoder.app.client.rpc.RPC;
+import org.cloudcoder.app.shared.model.ConfigurationSettingName;
+import org.cloudcoder.app.shared.model.User;
+import org.cloudcoder.app.shared.util.SubscriptionRegistrar;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * Login page.
+ */
 public class LoginPage extends CloudCoderPage {
-	private LoginPageUI ui;
+	/**
+	 * UI class for LoginPage.
+	 */
+	private class UI extends LayoutPanel implements SessionObserver {
+		private InlineLabel pageTitleLabel;
+		private TextBox usernameTextBox;
+		private PasswordTextBox passwordTextBox;
+		private InlineLabel errorLabel;
+		private InlineLabel loggingInLabel;
+		
+		public UI() {
+			//setSize("640px", "480px");
+			
+			InlineLabel usernameLabel = new InlineLabel("User name:");
+			add(usernameLabel);
+			setWidgetLeftWidth(usernameLabel, 57.0, Unit.PX, 91.0, Unit.PX);
+			setWidgetTopHeight(usernameLabel, 127.0, Unit.PX, 15.0, Unit.PX);
+			
+			usernameTextBox = new TextBox();
+			usernameTextBox.addKeyPressHandler(new KeyPressHandler() {
+				@Override
+				public void onKeyPress(KeyPressEvent event) {
+					if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) {
+						passwordTextBox.setFocus(true);
+					}
+				}
+			});
+			add(usernameTextBox);
+			setWidgetLeftWidth(usernameTextBox, 57.0, Unit.PX, 200.0, Unit.PX);
+			setWidgetTopHeight(usernameTextBox, 148.0, Unit.PX, 31.0, Unit.PX);
+			
+			InlineLabel passwordLabel = new InlineLabel("Password:");
+			add(passwordLabel);
+			setWidgetLeftWidth(passwordLabel, 57.0, Unit.PX, 91.0, Unit.PX);
+			setWidgetTopHeight(passwordLabel, 185.0, Unit.PX, 15.0, Unit.PX);
+			
+			passwordTextBox = new PasswordTextBox();
+			passwordTextBox.addKeyPressHandler(new KeyPressHandler() {
+				@Override
+				public void onKeyPress(KeyPressEvent event) {
+					if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) {
+						attemptLogin();
+					}
+				}
+			});
+			add(passwordTextBox);
+			setWidgetLeftWidth(passwordTextBox, 57.0, Unit.PX, 200.0, Unit.PX);
+			setWidgetTopHeight(passwordTextBox, 206.0, Unit.PX, 33.0, Unit.PX);
+			
+			Button loginButton = new Button("Log in");
+			loginButton.addClickHandler(new ClickHandler() {
+				public void onClick(ClickEvent event) {
+					attemptLogin();
+				}
+			});
+			add(loginButton);
+			setWidgetLeftWidth(loginButton, 176.0, Unit.PX, 81.0, Unit.PX);
+			setWidgetTopHeight(loginButton, 245.0, Unit.PX, 27.0, Unit.PX);
+			
+			Image cloudCoderLogoImage = new Image(GWT.getModuleBaseURL() + "images/CloudCoderLogo-med.png");
+			add(cloudCoderLogoImage);
+			setWidgetLeftWidth(cloudCoderLogoImage, 301.0, Unit.PX, 240.0, Unit.PX);
+			setWidgetTopHeight(cloudCoderLogoImage, 148.0, Unit.PX, 165.0, Unit.PX);
+			
+			InlineLabel promptLabel = new InlineLabel("Please enter your username and password.");
+			add(promptLabel);
+			setWidgetLeftWidth(promptLabel, 57.0, Unit.PX, 343.0, Unit.PX);
+			setWidgetTopHeight(promptLabel, 328.0, Unit.PX, 20.0, Unit.PX);
+			
+			errorLabel = new InlineLabel("");
+			errorLabel.setStylePrimaryName("cc-errorText");
+			add(errorLabel);
+			setWidgetLeftWidth(errorLabel, 57.0, Unit.PX, 484.0, Unit.PX);
+			setWidgetTopHeight(errorLabel, 383.0, Unit.PX, 73.0, Unit.PX);
+			
+			pageTitleLabel = new InlineLabel("");
+			pageTitleLabel.setStylePrimaryName("cc-pageTitle");
+			add(pageTitleLabel);
+			setWidgetLeftWidth(pageTitleLabel, 57.0, Unit.PX, 533.0, Unit.PX);
+			setWidgetTopHeight(pageTitleLabel, 44.0, Unit.PX, 31.0, Unit.PX);
+			
+			InlineLabel welcomeLabel = new InlineLabel("Welcome to CloudCoder at");
+			add(welcomeLabel);
+			setWidgetLeftWidth(welcomeLabel, 57.0, Unit.PX, 313.0, Unit.PX);
+			setWidgetTopHeight(welcomeLabel, 23.0, Unit.PX, 15.0, Unit.PX);
+			
+			loggingInLabel = new InlineLabel("");
+			add(loggingInLabel);
+			setWidgetLeftWidth(loggingInLabel, 57.0, Unit.PX, 343.0, Unit.PX);
+			setWidgetTopHeight(loggingInLabel, 353.0, Unit.PX, 20.0, Unit.PX);
+		}
+		
+		@Override
+		public void activate(Session session, SubscriptionRegistrar subscriptionRegistrar) {
+			// Load and display the institution name
+			RPC.configurationSettingService.getConfigurationSettingValue(ConfigurationSettingName.PUB_TEXT_INSTITUTION, new AsyncCallback<String>() {
+				@Override
+				public void onFailure(Throwable caught) {
+					// FIXME: display error
+				}
+				
+				@Override
+				public void onSuccess(String result) {
+					setPubTextInstitution(result);
+				}
+			});
+		}
+
+		protected void attemptLogin() {
+			String username = usernameTextBox.getText();
+			String password = passwordTextBox.getText();
+			if (username.equals("") || password.equals("")) {
+				errorLabel.setText("Please enter your username and password");
+			}
+			
+			loggingInLabel.setText("Logging in...");
+			RPC.loginService.login(username, password, new AsyncCallback<User>() {
+				@Override
+				public void onFailure(Throwable caught) {
+					loggingInLabel.setText("");
+					errorLabel.setText("Error communicating with server (are you connected to the network?)");
+				}
+				
+				@Override
+				public void onSuccess(User result) {
+					if (result == null) {
+						errorLabel.setText("Username and password not found");
+					} else {
+						// Successful login!
+						getSession().add(result);
+						getSession().notifySubscribers(Session.Event.LOGIN, result);
+					}
+				}
+			});
+		}
+
+		public void setPubTextInstitution(String result) {
+			pageTitleLabel.setText(result);
+			usernameTextBox.setFocus(true);
+		}
+	}
+
+	private UI ui;
 
 	@Override
 	public void createWidget() {
-		ui = new LoginPageUI();
-		ui.setPage(this);
+		ui = new UI();
 	}
 	
 	@Override
@@ -20,7 +185,7 @@ public class LoginPage extends CloudCoderPage {
 	}
 
 	@Override
-	public CloudCoderPageUI getWidget() {
+	public IsWidget getWidget() {
 		return ui;
 	}
 }
