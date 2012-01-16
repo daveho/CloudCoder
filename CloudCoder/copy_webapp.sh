a249255212c49f08022640e957b9f2ca6914df25
David Hovemeyer <david.hovemeyer@gmail.com>
2012-01-16 14:57:13 -0500

    by default, install into ../CloudCoderWebServer/apps directory

--- CloudCoder/copy_webapp.sh
+++ CloudCoder/copy_webapp.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 # Copy the web application to a destination directory.
+# By default, the destination directory is the "apps" subdirectory
+# of the CloudCoderWebServer project, which should be a sibling
+# of this (CloudCoder) project.
 
 todir="$1"
 
 if [ -z "$todir" ]; then
-	echo "Usage: ./copy_webapp.sh <todir>"
-	exit 1
+	todir=../CloudCoderWebServer/apps
 fi
 
 mkdir -p $todir/cloudCoder
