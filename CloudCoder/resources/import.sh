c354a7781ebe8e172609b12c84df86550d9a5820
David Hovemeyer <david.hovemeyer@gmail.com>
2012-01-26 16:46:00 -0500

    changed import scripts to facilitate integrating cc with marmoset
    The CloudCoder tables can exist alongside the Marmoset tables,
    and views expose the Marmoset user/courses/registrations tables
    in a form that CloudCoder can understand.

--- CloudCoder/resources/import.sh
+++ CloudCoder/resources/import.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
 dbname=cloudcoder
-
 dbuser=marmoset
 
-# This may fail if the database does not currently exist
-echo "drop database $dbname;" \
-	| mysql --user=$dbuser --pass
+if [ $# -ge 1 ]; then
+	dbname="$1"
+fi
+if [ $# -ge 2 ]; then
+	dbuser=marmoset
+fi
 
-(echo "create database $dbname; use $dbname; " && cat cloudcoder-schema.sql) \
+echo "dbname=$dbname"
+echo "dbuser=$dbuser"
+
+# Important: the database must already exist
+
+(echo "use $dbname; " && cat cloudcoder-schema.sql) \
 	| sed 's,InnoDB,MyISAM,' \
 	| mysql --user=$dbuser --pass
