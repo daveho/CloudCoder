97d8a4c31f80db65036a6a95795c60c5250cfe29
David Hovemeyer <david.hovemeyer@gmail.com>
2012-01-26 15:44:24 -0500

    added copyright notices/javadoc, minor cleanups

--- CloudCoder/src/org/cloudcoder/app/server/submitsvc/oop/ServerTask.java
+++ CloudCoder/src/org/cloudcoder/app/server/submitsvc/oop/ServerTask.java
@@ -1,3 +1,20 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package org.cloudcoder.app.server.submitsvc.oop;
 
 import java.io.IOException;
@@ -12,7 +29,12 @@ import org.cloudcoder.app.shared.model.SubmissionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+/**
+ * Server to listen for connections from remote Builder processes
+ * and start WorkerTask threads to communicate with them.
+ * 
+ * @author David Hovemeyer
+ */
 public class ServerTask implements Runnable {
     private static final Logger logger=LoggerFactory.getLogger(ServerTask.class);
     
@@ -73,7 +95,7 @@ public class ServerTask implements Runnable {
 		
 		} catch (IOException e) {
 			if (!shutdownRequested) {
-			    
+			    logger.error("IOException waiting for connections", e);
 			}
 		}
 	}
--- CloudCoder/src/org/cloudcoder/app/server/submitsvc/oop/Submission.java
+++ CloudCoder/src/org/cloudcoder/app/server/submitsvc/oop/Submission.java
@@ -1,6 +1,22 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package org.cloudcoder.app.server.submitsvc.oop;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.cloudcoder.app.server.submitsvc.SubmissionException;
@@ -8,6 +24,12 @@ import org.cloudcoder.app.shared.model.Problem;
 import org.cloudcoder.app.shared.model.SubmissionResult;
 import org.cloudcoder.app.shared.model.TestCase;
 
+/**
+ * A Submission (Problem, TestCases, and program text)
+ * waiting to be tested by a remote Builder.
+ *  
+ * @author David Hovemeyer
+ */
 public class Submission {
 	private Object lock = new Object();
 	private Problem problem;
