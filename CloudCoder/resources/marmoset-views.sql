af867a6d9ac63ee381f35812accc1b0e0201112b
David Hovemeyer <david.hovemeyer@gmail.com>
2012-01-27 12:01:40 -0500

    improved cc_courses view so it produces correct term ids and years
    Assumes that marmoset semester values are in the form
    'Fall 2011', 'Spring 2012', etc.

--- CloudCoder/resources/marmoset-views.sql
+++ CloudCoder/resources/marmoset-views.sql
@@ -8,14 +8,20 @@ create view cc_users as
          from students;
 
 -- expose marmoset courses table as CloudCoder cc_courses table
+-- Assumes semester values are in the form 'Fall 2011', 'Spring 2012', etc.
 create view cc_courses as
   select courses.course_pk as id,
          courses.coursename as name,
          courses.description as title,
          courses.url as url,
-         2 as term_id,                  -- XXX: hardcoded term "Spring"
-         2012 as year                -- XXX: hardcoded year
-         from courses;
+         CASE substring(courses.semester, 1, locate(' ', courses.semester) - 1)
+           when 'Fall' then 6
+           when 'Winter' then 1
+           when 'Spring' then 2
+           when 'Summer' then 3
+           else 2 END as term_id,
+         convert(substring(courses.semester from locate(' ', courses.semester) + 1), signed) as year
+   from courses;
 
 -- expose marmoset student_registration table as CloudCoder cc_course_registrations table
 create view cc_course_registrations as
