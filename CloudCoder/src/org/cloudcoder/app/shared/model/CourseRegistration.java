bbab4db7fd941af0ea54eed5f9323569e242c782
David Hovemeyer <david.hovemeyer@gmail.com>
2012-02-04 07:38:44 -0500

    minor code cleanup; remove unused JPAs, add javadoc

--- CloudCoder/src/org/cloudcoder/app/shared/model/CourseRegistration.java
+++ CloudCoder/src/org/cloudcoder/app/shared/model/CourseRegistration.java
@@ -1,6 +1,21 @@
-package org.cloudcoder.app.shared.model;
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//import javax.persistence.Column;
+package org.cloudcoder.app.shared.model;
 //import javax.persistence.Entity;
 //import javax.persistence.GeneratedValue;
 //import javax.persistence.GenerationType;
@@ -9,23 +24,18 @@ package org.cloudcoder.app.shared.model;
 
 import java.io.Serializable;
 
-//@Entity
-//@Table(name="course_registrations")
+/**
+ * Model object representing the registration of a
+ * {@link User} in a {@link Course}.
+ * 
+ * @author David Hovemeyer
+ */
 public class CourseRegistration implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-//	@Column(name="id")
 	private int id;
-	
-//	@Column(name="course_id")
 	private int courseId;
-	
-//	@Column(name="user_id")
 	private int userId;
-	
-//	@Column(name="registration_type")
 	private int registrationType;
 	
 	public CourseRegistration() {
