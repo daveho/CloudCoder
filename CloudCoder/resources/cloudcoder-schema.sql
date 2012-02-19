e71acfbbc16ced628d6df4e896aec23e30fc35b1
David Hovemeyer <david.hovemeyer@gmail.com>
2012-02-19 12:42:22 -0500

    make InnoDB default storage engine, utf8_general_ci default collation

--- CloudCoder/resources/cloudcoder-schema.sql
+++ CloudCoder/resources/cloudcoder-schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 19, 2012 at 08:13 AM
+-- Generation Time: Feb 19, 2012 at 12:41 PM
 -- Server version: 5.1.58
 -- PHP Version: 5.3.6-13ubuntu3.6
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `cc_changes` (
   `end_col` mediumint(9) NOT NULL,
   `text` text NOT NULL,
   UNIQUE KEY `event_id` (`event_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `cc_configuration_settings` (
   `name` varchar(60) NOT NULL,
   `value` text NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `cc_configuration_settings`
@@ -64,13 +64,13 @@ INSERT INTO `cc_configuration_settings` (`name`, `value`) VALUES
 
 CREATE TABLE IF NOT EXISTS `cc_courses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `url` varchar(120) NOT NULL,
+  `name` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `title` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `url` varchar(120) CHARACTER SET latin1 NOT NULL,
   `term_id` int(11) NOT NULL,
   `year` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `cc_courses`
@@ -92,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `cc_course_registrations` (
   `registration_type` int(11) NOT NULL,
   `section` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `cc_course_registrations`
@@ -120,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `cc_events` (
   PRIMARY KEY (`id`),
   KEY `problem_id` (`problem_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -139,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `cc_problems` (
   `when_due` bigint(20) NOT NULL,
   `skeleton` varchar(400) DEFAULT NULL,
   PRIMARY KEY (`problem_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -154,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `cc_submission_receipts` (
   `num_tests_attempted` int(11) NOT NULL,
   `num_tests_passed` int(11) NOT NULL,
   UNIQUE KEY `event_id` (`event_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -167,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `cc_terms` (
   `name` varchar(20) NOT NULL,
   `seq` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `cc_terms`
@@ -196,7 +196,7 @@ CREATE TABLE IF NOT EXISTS `cc_test_cases` (
   `secret` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`test_case_id`),
   KEY `problem_id` (`problem_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=124 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=124 ;
 
 --
 -- Dumping data for table `cc_test_cases`
@@ -341,7 +341,7 @@ CREATE TABLE IF NOT EXISTS `cc_test_results` (
   `stderr` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `submission_receipt_id` (`submission_receipt_event_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -351,12 +351,12 @@ CREATE TABLE IF NOT EXISTS `cc_test_results` (
 
 CREATE TABLE IF NOT EXISTS `cc_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
-  `password_md5` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
-  `salt` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password_md5` varchar(32) DEFAULT NULL,
+  `salt` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_username_index` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=ucs2 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `cc_users`
