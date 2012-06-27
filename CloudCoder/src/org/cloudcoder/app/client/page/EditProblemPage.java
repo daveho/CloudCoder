3d29cf6b9f7fd9704e641bf60f344d9371f52c30
David Hovemeyer <david.hovemeyer@gmail.com>
2012-06-27 13:17:44 -0400

    started EditProblemPage, a UI for editing problem / test cases

---
+++ CloudCoder/src/org/cloudcoder/app/client/page/EditProblemPage.java
@@ -0,0 +1,97 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.client.page;
+
+import org.cloudcoder.app.client.model.Session;
+import org.cloudcoder.app.shared.model.Problem;
+import org.cloudcoder.app.shared.model.TestCase;
+import org.cloudcoder.app.shared.util.Publisher;
+import org.cloudcoder.app.shared.util.Subscriber;
+import org.cloudcoder.app.shared.util.SubscriptionRegistrar;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.ResizeComposite;
+
+/**
+ * Page for editing a {@link Problem} and its associated {@link TestCase}s.
+ * 
+ * @author David Hovemeyer
+ */
+public class EditProblemPage extends CloudCoderPage {
+	
+	private class UI extends ResizeComposite implements SessionObserver, Subscriber {
+
+		/* (non-Javadoc)
+		 * @see org.cloudcoder.app.client.page.SessionObserver#activate(org.cloudcoder.app.client.model.Session, org.cloudcoder.app.shared.util.SubscriptionRegistrar)
+		 */
+		@Override
+		public void activate(Session session, SubscriptionRegistrar subscriptionRegistrar) {
+		}
+
+		/* (non-Javadoc)
+		 * @see org.cloudcoder.app.shared.util.Subscriber#eventOccurred(java.lang.Object, org.cloudcoder.app.shared.util.Publisher, java.lang.Object)
+		 */
+		@Override
+		public void eventOccurred(Object key, Publisher publisher, Object hint) {
+		}
+		
+	}
+	
+	private UI ui;
+
+	/* (non-Javadoc)
+	 * @see org.cloudcoder.app.client.page.CloudCoderPage#createWidget()
+	 */
+	@Override
+	public void createWidget() {
+		ui = new UI();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.cloudcoder.app.client.page.CloudCoderPage#activate()
+	 */
+	@Override
+	public void activate() {
+		ui.activate(getSession(), getSubscriptionRegistrar());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.cloudcoder.app.client.page.CloudCoderPage#deactivate()
+	 */
+	@Override
+	public void deactivate() {
+		getSubscriptionRegistrar().cancelAllSubscriptions();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.cloudcoder.app.client.page.CloudCoderPage#getWidget()
+	 */
+	@Override
+	public IsWidget getWidget() {
+		return ui;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.cloudcoder.app.client.page.CloudCoderPage#isActivity()
+	 */
+	@Override
+	public boolean isActivity() {
+		return true;
+	}
+	
+}
