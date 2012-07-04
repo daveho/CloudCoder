28ad6f2ed9f16a8bc75317e3ca5ad14a84065451
David Hovemeyer <david.hovemeyer@gmail.com>
2012-07-04 14:02:06 -0400

    added EditProblemAdapter, for notification when an IProblem is modified
    Factored it out of EditProblemPage.

---
+++ CloudCoderModelClasses/src/org/cloudcoder/app/shared/model/EditProblemAdapter.java
@@ -0,0 +1,222 @@
+package org.cloudcoder.app.shared.model;
+
+import java.util.Date;
+
+/**
+ * Adapter class for editing an {@link IProblem}.
+ * Subclasses must implement the {@link #onChange()} method,
+ * which is called whenever the underlying IProblem object is
+ * modified with a setter method.
+ */
+public abstract class EditProblemAdapter implements IProblem {
+	private final IProblem delegate;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param delegate an IProblem object to which getter/setter calls will be delegated
+	 */
+	public EditProblemAdapter(IProblem delegate) {
+		this.delegate = delegate;
+	}
+
+	/**
+	 * Downcall method: is called whenever a setter method has been called
+	 * and the IProblem object to which the adapter is delegating
+	 * might have changed.
+	 */
+	protected abstract void onChange();
+
+	@Override
+	public void setProblemType(ProblemType problemType) {
+		delegate.setProblemType(problemType);
+		onChange();
+	}
+
+	@Override
+	public void setProblemType(int problemType) {
+		delegate.setProblemType(problemType);
+		onChange();
+	}
+
+	@Override
+	public ProblemType getProblemType() {
+		return delegate.getProblemType();
+	}
+
+	@Override
+	public String getTestName() {
+		return delegate.getTestName();
+	}
+
+	@Override
+	public void setTestName(String testName) {
+		delegate.setTestName(testName);
+		onChange();
+	}
+
+	@Override
+	public void setBriefDescription(String briefDescription) {
+		delegate.setBriefDescription(briefDescription);
+		onChange();
+	}
+
+	@Override
+	public String getBriefDescription() {
+		return delegate.getBriefDescription();
+	}
+
+	@Override
+	public String getDescription() {
+		return delegate.getDescription();
+	}
+
+	@Override
+	public void setDescription(String description) {
+		delegate.setDescription(description);
+		onChange();
+	}
+
+	@Override
+	public void setSkeleton(String skeleton) {
+		delegate.setSkeleton(skeleton);
+		onChange();
+	}
+
+	@Override
+	public String getSkeleton() {
+		return delegate.getSkeleton();
+	}
+
+	@Override
+	public void setSchemaVersion(int schemaVersion) {
+		delegate.setSchemaVersion(schemaVersion);
+		onChange();
+	}
+
+	@Override
+	public int getSchemaVersion() {
+		return delegate.getSchemaVersion();
+	}
+
+	@Override
+	public void setAuthorName(String authorName) {
+		delegate.setAuthorName(authorName);
+		onChange();
+	}
+
+	@Override
+	public String getAuthorName() {
+		return delegate.getAuthorName();
+	}
+
+	@Override
+	public void setAuthorEmail(String authorEmail) {
+		delegate.setAuthorEmail(authorEmail);
+		onChange();
+	}
+
+	@Override
+	public String getAuthorEmail() {
+		return delegate.getAuthorEmail();
+	}
+
+	@Override
+	public void setAuthorWebsite(String authorWebsite) {
+		delegate.setAuthorWebsite(authorWebsite);
+		onChange();
+	}
+
+	@Override
+	public String getAuthorWebsite() {
+		return delegate.getAuthorWebsite();
+	}
+
+	@Override
+	public void setTimestampUTC(long timestampUTC) {
+		delegate.setTimestampUTC(timestampUTC);
+		onChange();
+	}
+
+	@Override
+	public long getTimestampUTC() {
+		return delegate.getTimestampUTC();
+	}
+
+	@Override
+	public void setLicense(ProblemLicense license) {
+		delegate.setLicense(license);
+		onChange();
+	}
+
+	@Override
+	public ProblemLicense getLicense() {
+		return delegate.getLicense();
+	}
+
+	@Override
+	public Integer getProblemId() {
+		return delegate.getProblemId();
+	}
+
+	@Override
+	public void setProblemId(Integer id) {
+		delegate.setProblemId(id);
+		onChange();
+	}
+
+	@Override
+	public Integer getCourseId() {
+		return delegate.getCourseId();
+	}
+
+	@Override
+	public void setCourseId(Integer courseId) {
+		delegate.setCourseId(courseId);
+		onChange();
+	}
+
+	@Override
+	public long getWhenAssigned() {
+		return delegate.getWhenAssigned();
+	}
+
+	@Override
+	public Date getWhenAssignedAsDate() {
+		return delegate.getWhenAssignedAsDate();
+	}
+
+	@Override
+	public void setWhenAssigned(long whenAssigned) {
+		delegate.setWhenAssigned(whenAssigned);
+		onChange();
+	}
+
+	@Override
+	public long getWhenDue() {
+		return delegate.getWhenDue();
+	}
+
+	@Override
+	public Date getWhenDueAsDate() {
+		return delegate.getWhenDueAsDate();
+	}
+
+	@Override
+	public void setWhenDue(long whenDue) {
+		delegate.setWhenDue(whenDue);
+		onChange();
+	}
+
+	@Override
+	public void setVisible(boolean visible) {
+		delegate.setVisible(visible);
+		onChange();
+	}
+
+	@Override
+	public boolean isVisible() {
+		return delegate.isVisible();
+	}
+
+}
