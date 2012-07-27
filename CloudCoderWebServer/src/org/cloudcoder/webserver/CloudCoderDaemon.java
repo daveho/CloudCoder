46a6714f1c7df09b9eac3b03e4d601263906cb0c
David Hovemeyer <david.hovemeyer@gmail.com>
2012-07-27 11:38:41 -0400

    use override-web.xml to override ctx params, build script improvements
    Jetty allows overriding of web.xml context params by specifying
    an additional web.xml whose values override the one in the
    webapp's actual web.xml.  We now use this mechanism to allow
    the configuration properties in local.properties (generated by
    configure.pl) to override the ones in web.xml, which are suitable
    only for development.
    
    Also, improvements to build.xml scripts.  Specifically,
    we don't copy cloudcoderModelClasses.jar and
    cloudcoderModelClassesPersist.jar into the webapp's
    WEB-INF/lib directory any more, but instead resolve build-time
    and development-time dependencies on them through the
    classpath.  CloudCoderWebServer's build.xml copies these
    jar files into WEB-INF/lib as part of creating
    cloudcoderApp.jar, so they are never in the actual
    filesystem.  This is important, because a stale version
    of either jar file at development time can lead to strange
    behavior, as the source code can get out of sync with the
    class files (in the jars) that the webapp is actually
    using.
    
    Also, modified the build.xmls so that needed libraries and
    code are built automatically, for proper prerequisite
    management.

--- CloudCoderWebServer/src/org/cloudcoder/webserver/CloudCoderDaemon.java
+++ CloudCoderWebServer/src/org/cloudcoder/webserver/CloudCoderDaemon.java
@@ -1,6 +1,29 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package org.cloudcoder.webserver;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.log4j.PropertyConfigurator;
@@ -10,7 +33,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of {@link IDaemon} to start the CloudCoder web application
@@ -57,29 +79,17 @@ public class CloudCoderDaemon implements IDaemon {
 		Properties configProperties = loadProperties("local.properties");
 		CloudCoderConfig config = new CloudCoderConfig(configProperties);
 		
-		// Set "overrides" to cloudcoder.* properties that are defined in the
-		// webapp's web.xml.  Specifically, we need this for database configuration
-		// properties, because the ones in web.xml are hardcoded for development,
-		// and instead we want to use the ones in the embedded config properties.
-		// Unfortunately, Jetty makes it nearly impossible to do arbitrary
-		// configuration of servlet context init params.  So, as a hack,
-		// we stuff them into a system property where JDBCDatabaseConfigServletContextListener
-		// will be able to see them.
-		StringBuilder buf = new StringBuilder();
-		for (String key : configProperties.stringPropertyNames()) {
-			if (key.startsWith("cloudcoder.")) {
-				String value = configProperties.getProperty(key);
-				if (buf.length() > 0) {
-					buf.append("|");
-				}
-				buf.append(key);
-				buf.append("=");
-				buf.append(value);
-			}
+		// Create an override-web.xml to override context parameters specified in the
+		// webapp's web.xml.  Its web.xml contains configuration values appropriate
+		// for development (which is useful), but we want the configuration values
+		// specified for deployment when the user ran the configure.pl script
+		// (in local.properties).
+		String overrideWebXml;
+		try {
+			overrideWebXml = createOverrideWebXml(configProperties);
+		} catch (IOException e) {
+			throw new IllegalStateException("Couldn't create override-web.xml", e);
 		}
-		String overrides = buf.toString();
-		LoggerFactory.getLogger(this.getClass()).info("Setting config prop overrides: " + overrides);
-		System.setProperty("cloudcoder.config", overrides);
 		
 		// Create an embedded Jetty server
 		this.server = new Server();
@@ -107,6 +117,9 @@ public class CloudCoderDaemon implements IDaemon {
 			handler.setWar(codeBase + (endsInDir ? "" : "/") + "war");
 		}
 		handler.setContextPath(config.getContext());
+		
+		// Configure the override-web.xml
+		handler.setOverrideDescriptors(Arrays.asList(overrideWebXml));
 
 		// Add it to the server
 		server.setHandler(handler);
@@ -123,6 +136,42 @@ public class CloudCoderDaemon implements IDaemon {
 		}
 	}
 	
+	private String createOverrideWebXml(Properties configProperties) throws IOException {
+		// It is somewhat unfortunate that Jetty doesn't allow overriding
+		// context parameters through a programmatic API: that would
+		// be much easier than having to create a file.  However,
+		// we can at least be thankful that it is possible to override
+		// context parameters at all, since it greatly simplifies the
+		// process of configuring CloudCoder for deployment.
+		
+		// Create a temp file
+		File f = File.createTempFile("ccws", ".xml");
+		//f.deleteOnExit();
+		
+		FileOutputStream fos = new FileOutputStream(f);
+		PrintWriter w = new PrintWriter(new OutputStreamWriter(fos, "UTF-8"));
+		try {
+			w.println("<?xml version=\"1.0\"  encoding=\"UTF-8\"?>");
+			w.println("<web-app>");
+			
+			for (String prop : configProperties.stringPropertyNames()) {
+				if (!prop.startsWith("cloudcoder.")) {
+					continue;
+				}
+				w.println("  <context-param>");
+				w.println("    <param-name>" + prop + "</param-name>");
+				w.println("    <param-value>" + configProperties.getProperty(prop) + "</param-value>");
+				w.println("  </context-param>");
+			}
+			
+			w.println("</web-app>");
+		} finally {
+			w.close();
+		}
+		
+		return f.getAbsolutePath();
+	}
+
 	private void configureLogging() {
 		Properties log4jProperties = loadProperties("log4j.properties");
 		PropertyConfigurator.configure(log4jProperties);
