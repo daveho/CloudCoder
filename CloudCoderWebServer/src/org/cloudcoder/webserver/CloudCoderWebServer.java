9c8087e9760b3c08c6805156b82bb66d633cf3c9
David Hovemeyer <david.hovemeyer@gmail.com>
2012-07-31 11:01:30 -0400

    allow CreateWebappDatabase to run from deployable jarfile
    This will simplify deployment, and gives us a nice way to
    allow the user to customize their installation, because
    CreateWebappDatabase can prompt the user for values
    such as the institution name.  In the future, we could also
    allow other administrative kinds of tasks to be done
    by the deployable jarfile.

--- CloudCoderWebServer/src/org/cloudcoder/webserver/CloudCoderWebServer.java
+++ CloudCoderWebServer/src/org/cloudcoder/webserver/CloudCoderWebServer.java
@@ -17,6 +17,10 @@
 
 package org.cloudcoder.webserver;
 
+import java.lang.reflect.Method;
+import java.security.ProtectionDomain;
+import java.util.jar.JarFile;
+
 /**
  * Main entry point for the self-contained single-jar-file
  * deployment for the CloudCoder web application.
@@ -24,8 +28,55 @@ package org.cloudcoder.webserver;
  * @author David Hovemeyer
  */
 public class CloudCoderWebServer {
-	public static void main(String[] args) {
-		CloudCoderDaemonController controller = new CloudCoderDaemonController();
-		controller.exec(args);
+	public static void main(String[] args) throws Exception {
+		// If the command is "createdb", then execute the CreateWebappDatabase
+		// program from the CloudCoderModelClasesPersistence library.
+		// Otherwise, use the CloudCoderDaemonController to handle the command.
+		if (args.length == 1 && args[0].equals("createdb")) {
+			createWebappDatabase();
+		} else {
+			// CloudCoderDaemonController handles requests to start/control/shutdown
+			// the webapp (and its web server).
+			CloudCoderDaemonController controller = new CloudCoderDaemonController();
+			controller.exec(args);
+		}
+	}
+	
+	/**
+	 * Run the CreateWebappDatabase program by loading it with a classloader
+	 * that can load classes and resources from nested jarfiles.
+	 * This is kind of ugly, but single-jarfile deployment is a
+	 * beautiful thing, and this gives us the ability to run code
+	 * out of the web application nested in the deployable jarfile.
+	 * 
+	 * @throws Exception
+	 */
+	private static void createWebappDatabase() throws Exception {
+		ProtectionDomain p = CloudCoderWebServer.class.getProtectionDomain();
+		String codeBase = p.getCodeSource().getLocation().toExternalForm();
+		
+		// Get path of executable jar file
+		if (!codeBase.startsWith("file:") || !codeBase.endsWith(".jar")) {
+			throw new IllegalStateException("Codebase " + codeBase + " not a jarfile");
+		}
+		String jarPath = codeBase.substring("file:".length());
+		//System.out.println(jarPath);
+		
+		// Create a JarFile object to read from the executable jarfile.
+		JarFile jarFile = new JarFile(jarPath);
+		
+		try {
+			// A NestedJarClassLoader will allow CreateWebappDatabase to run out of
+			// the cloudcoderModelClassesPersist.jar nested in the executable jarfile,
+			// along with its dependencies, some of which are also nested jarfiles. 
+			NestedJarClassLoader classLoader = new NestedJarClassLoader(jarFile, CloudCoderWebServer.class.getClassLoader());
+			
+			// Load and run CreateWebappDatabase's main via reflection.
+			Class<?> createWebappDatabase = classLoader.loadClass("org.cloudcoder.app.server.persist.CreateWebappDatabase");
+			Method main = createWebappDatabase.getMethod("main", new Class<?>[]{String[].class});
+			main.invoke(null, new Object[]{new String[0]});
+		} finally {
+			jarFile.close();
+		}
 	}
 }
