97464cf58fdb01d4051c571e16c799de83ead92b
David Hovemeyer <david.hovemeyer@gmail.com>
2012-09-25 16:10:22 -0400

    added a filter to prevent cacheing of .nocache.js files

---
+++ CloudCoder/src/org/cloudcoder/app/server/filter/NocacheJsFilter.java
@@ -0,0 +1,61 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.server.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Prevent caching of ".nocache.js" resources.
+ * 
+ * @author David Hovemeyer
+ */
+public class NocacheJsFilter implements Filter {
+	private static final Logger logger = LoggerFactory.getLogger(NocacheJsFilter.class);
+
+	@Override
+	public void doFilter(ServletRequest req_, ServletResponse resp_, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse resp = (HttpServletResponse) resp_;
+		logger.info("Adding Cache-Control header to {}", ((HttpServletRequest)req_).getRequestURI());
+		resp.addHeader("Cache-Control", "max-age=0");
+		chain.doFilter(req_, resp_);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		// nothing to do
+	}
+
+	@Override
+	public void destroy() {
+		// nothing to do
+	}
+
+}
