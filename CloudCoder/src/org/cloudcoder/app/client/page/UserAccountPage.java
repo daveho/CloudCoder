a959ba5b18bc703bb3f78aad9f89edddf933975b
Jaime Spacco <jspacco@knox.edu>
2012-10-25 22:39:50 -0500

    commiting some of Andrei's changes before doing a merge.  This merge involved CRLF eols getting into Andrei's repository, probably due to the Windows git client using a stupid default for autocrlf, and that has made this a painful multi-hour merge

---
+++ CloudCoder/src/org/cloudcoder/app/client/page/UserAccountPage.java
@@ -0,0 +1,440 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.client.page;
+
+import org.cloudcoder.app.client.model.Session;
+import org.cloudcoder.app.client.rpc.RPC;
+import org.cloudcoder.app.client.view.PageNavPanel;
+import org.cloudcoder.app.client.view.StatusMessageView;
+import org.cloudcoder.app.client.view.UserAccountView;
+import org.cloudcoder.app.client.view.UserProgressListView;
+import org.cloudcoder.app.shared.model.Course;
+import org.cloudcoder.app.shared.model.CourseRegistrationType;
+import org.cloudcoder.app.shared.model.User;
+import org.cloudcoder.app.shared.util.Publisher;
+import org.cloudcoder.app.shared.util.Subscriber;
+import org.cloudcoder.app.shared.util.SubscriptionRegistrar;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author jspacco
+ * @author apapance
+ *
+ */
+public class UserAccountPage extends CloudCoderPage
+{
+    private static final long serialVersionUID = 1L;
+    
+    private enum ButtonPanelAction {
+    	EDIT("Edit account"),
+    	VIEW_PROGRESS("View progress in course");
+        
+        private String name;
+        
+        private ButtonPanelAction(String name) {
+            this.name = name;
+        }
+        
+        /**
+         * @return the name
+         */
+        public String getName() {
+            return name;
+        }
+        
+    }
+    private class UI extends Composite implements SessionObserver, Subscriber {
+        private static final double USERS_BUTTON_BAR_HEIGHT_PX = 28.0;
+
+        private PageNavPanel pageNavPanel;
+        private String rawCourseTitle;
+        private Label courseLabel;
+        private Button[] userManagementButtons;
+        private UserAccountView userAccountView;
+        private StatusMessageView statusMessageView;
+        
+        public UI() {
+            DockLayoutPanel dockLayoutPanel = new DockLayoutPanel(Unit.PX);
+            
+            // Create a north panel with course info and a PageNavPanel
+            LayoutPanel northPanel = new LayoutPanel();
+            this.courseLabel = new Label();
+            northPanel.add(courseLabel);
+            northPanel.setWidgetLeftRight(courseLabel, 0.0, Unit.PX, PageNavPanel.WIDTH, PageNavPanel.WIDTH_UNIT);
+            northPanel.setWidgetTopHeight(courseLabel, 0.0, Unit.PX, PageNavPanel.HEIGHT, PageNavPanel.HEIGHT_UNIT);
+            courseLabel.setStyleName("cc-courseLabel");
+            
+            this.pageNavPanel = new PageNavPanel();
+            northPanel.add(pageNavPanel);
+            northPanel.setWidgetRightWidth(pageNavPanel, 0.0, Unit.PX, PageNavPanel.WIDTH, PageNavPanel.WIDTH_UNIT);
+            northPanel.setWidgetTopHeight(pageNavPanel, 0.0, Unit.PX, PageNavPanel.HEIGHT, PageNavPanel.HEIGHT_UNIT);
+            
+            dockLayoutPanel.addNorth(northPanel, PageNavPanel.HEIGHT);
+            
+            // Create a center panel with user button panel and list of users 
+            // registered for the given course.
+            // Can eventually put other stuff here too.
+            LayoutPanel centerPanel = new LayoutPanel();
+            
+            // Create a button panel with buttons for problem-related actions
+            // (new problem, edit problem, make visible, make invisible, quiz, share)
+            FlowPanel userButtonPanel = new FlowPanel();
+            ButtonPanelAction[] actions = ButtonPanelAction.values();
+            userManagementButtons = new Button[actions.length];
+            for (final ButtonPanelAction action : actions) {
+                final Button button = new Button(action.getName());
+                userManagementButtons[action.ordinal()] = button;
+                button.addClickHandler(new ClickHandler() {
+                    @Override
+                    public void onClick(ClickEvent event) {
+                        switch (action) {
+                        case EDIT:
+                            handleEditUser(event);
+                            break;
+                            
+                        case VIEW_PROGRESS:
+                            handleUserProgress(event);
+                            break;
+                        }                    }
+                });
+                button.setEnabled(false);
+                userButtonPanel.add(button);
+            }
+            
+            centerPanel.add(userButtonPanel);
+            centerPanel.setWidgetTopHeight(userButtonPanel, 0.0, Unit.PX, 28.0, Unit.PX);
+            centerPanel.setWidgetLeftRight(userButtonPanel, 0.0, Unit.PX, 0.0, Unit.PX);
+            
+            // Create users list
+            this.userAccountView = new UserAccountView();
+            centerPanel.add(userAccountView);
+            centerPanel.setWidgetTopBottom(userAccountView, USERS_BUTTON_BAR_HEIGHT_PX, Unit.PX, StatusMessageView.HEIGHT_PX, Unit.PX);
+            centerPanel.setWidgetLeftRight(userAccountView, 0.0, Unit.PX, 0.0, Unit.PX);
+            
+            // Create a StatusMessageView
+            this.statusMessageView = new StatusMessageView();
+            centerPanel.add(statusMessageView);
+            centerPanel.setWidgetBottomHeight(statusMessageView, 0.0, Unit.PX, StatusMessageView.HEIGHT_PX, Unit.PX);
+            centerPanel.setWidgetLeftRight(statusMessageView, 0.0, Unit.PX, 0.0, Unit.PX);
+            
+            dockLayoutPanel.add(centerPanel);
+            
+            initWidget(dockLayoutPanel);
+        }
+        
+        /**
+         * @author Andrei Papancea
+         *
+         * View a particular user's progress throughout the course.
+         * The pop-up will display the problems that the user has started
+         * and their status (complete/incomplete, num_tests_passed/num_tests_total).
+         * 
+         */
+        private class UserProgressPopupPanel extends PopupPanel{
+        	
+        	public UserProgressPopupPanel(final Widget widget, final User user, 
+                    final Course course, final CourseRegistrationType originalType, final Session session)
+            {
+               super(true);
+               
+               VerticalPanel vp = new VerticalPanel();
+               
+               setWidget(vp);
+               
+               vp.setWidth("600px");
+               
+               //vp.setSize("600px", "400px");      
+
+               vp.add(new HTML("Problem statistics for <b>"+
+            		   			user.getFirstname()+" "+user.getLastname()+" ("+
+            		   			user.getUsername()+")</b><br /><br />"));
+               
+               UserProgressListView myGrid = new UserProgressListView(user);
+               myGrid.activate(session, getSubscriptionRegistrar());
+               vp.add(myGrid);
+            }        	
+        }
+  
+        private class EditUserPopupPanel extends PopupPanel{
+
+            public EditUserPopupPanel(final Widget widget, final User user, 
+                    final Course course, final CourseRegistrationType originalType)
+            {
+               super(true);
+               
+               VerticalPanel vp = new VerticalPanel();
+
+               setWidget(vp);
+               
+               final FormPanel form = new FormPanel();
+               // We won't actually submit the form to a servlet
+               // instead we intercept the form fields
+               // and make an async call
+
+               // TODO are these style file hooks?
+               form.addStyleName("table-center");
+               form.addStyleName("demo-FormPanel");
+
+               VerticalPanel holder = new VerticalPanel();
+
+               holder.add(new HTML(new SafeHtmlBuilder().appendEscapedLines("Change the fields you want to edit.\n" +
+               		"Any fields left blank will be unchanged\n\n").toSafeHtml()));
+               
+               // username
+               holder.add(new HTML("Username:<br /><b>"+user.getUsername()+"</b><br /><br />"));
+               
+               // firstname
+               holder.add(new Label("Firstname"));
+               final TextBox firstname = new TextBox();
+               firstname.setName("firstname");
+               firstname.setValue(user.getFirstname());
+               holder.add(firstname);
+               
+               // lastname
+               holder.add(new Label("Lastname"));
+               final TextBox lastname = new TextBox();
+               lastname.setName("lastname");
+               lastname.setValue(user.getLastname());
+               holder.add(lastname);
+               
+               // email
+               holder.add(new Label("Email"));
+               final TextBox email = new TextBox();
+               email.setName("email");
+               email.setValue(user.getEmail());
+               holder.add(email);
+               
+               // password
+               holder.add(new Label("Password"));
+               final PasswordTextBox passwd = new PasswordTextBox();
+               passwd.setName("passwd");
+               holder.add(passwd);
+               
+               // re-enter password
+               holder.add(new Label("re-enter Password"));
+               final PasswordTextBox passwd2 = new PasswordTextBox();
+               passwd2.setName("passwd2");
+               holder.add(passwd2);
+               
+               // radio button for the account type
+               holder.add(new Label("Allow anonymous collection of your coding data:"));
+               final RadioButton studentAccountButton = new RadioButton("consent","Allow");
+               studentAccountButton.setValue(true);
+               final RadioButton instructorAccountButton = new RadioButton("consent","Do NOT allow");
+               holder.add(studentAccountButton);
+               holder.add(instructorAccountButton);
+               
+               form.add(holder);
+               vp.add(form);
+               
+               final PopupPanel panelCopy=this;
+               
+               holder.add(new Button("Edit user", new ClickHandler() {
+                   @Override
+                   public void onClick(ClickEvent event) {
+                       //This is more like a fake form
+                       //we're not submitting it to a server-side servlet
+                       GWT.log("edit user submit clicked");
+                       final User user=getSession().get(User.class);
+                       String username = user.getUsername();
+                       
+                       if (!user.getUsername().equals(username) ||
+                               user.getFirstname().equals(firstname.getValue()) ||
+                               user.getLastname().equals(lastname.getValue()) ||
+                               user.getEmail().equals(email.getValue()) ||
+                               passwd.getValue().length()>0)
+                       {
+                           if (!passwd.getValue().equals(passwd2.getValue())) {
+                               Window.alert("Passwords do no match");
+                               return;
+                           }
+                           // at least one field was edited
+                           GWT.log("user id is "+user.getId());
+                           GWT.log("username from the session is "+user.getUsername());
+                           RPC.usersService.editUser(user.getId(), 
+                                   username, 
+                                   firstname.getValue(), 
+                                   lastname.getValue(), 
+                                   email.getValue(), 
+                                   passwd.getValue(),
+                                   new AsyncCallback<Boolean>()
+                           { 
+                               @Override
+                               public void onSuccess(Boolean result) {
+                                   GWT.log("Edited "+user.getUsername()+" in course "+rawCourseTitle);
+                                   panelCopy.hide();
+                                   Window.alert("Successfully edited user record");
+                                   reloadUser();
+                               }
+
+                               @Override
+                               public void onFailure(Throwable caught) {
+                                   GWT.log("Failed to edit student");
+                                   Window.alert("Unable to edit "+user.getUsername()+" in course "+rawCourseTitle);
+                               }
+                           });
+                       } else {
+                           panelCopy.hide();
+                           Window.alert("Nothing was changed");
+                       }
+                   }
+               }));
+               
+            }
+        }
+        
+        /* (non-Javadoc)
+         * @see org.cloudcoder.app.client.page.SessionObserver#activate(org.cloudcoder.app.client.model.Session, org.cloudcoder.app.shared.util.SubscriptionRegistrar)
+         */
+        public void activate(Session session, SubscriptionRegistrar subscriptionRegistrar) {
+            session.subscribe(Session.Event.ADDED_OBJECT, this, subscriptionRegistrar);
+            
+            // Activate views
+            pageNavPanel.setBackHandler(new BackHomeHandler(session));
+            pageNavPanel.setLogoutHandler(new LogoutHandler(session));
+            userAccountView.activate(session, subscriptionRegistrar);
+            statusMessageView.activate(session, subscriptionRegistrar);
+            
+            // The session should contain a course
+            Course course = session.get(Course.class);
+            rawCourseTitle=course.getName()+" - "+course.getTitle();
+            courseLabel.setText(rawCourseTitle);
+            session.subscribe(Session.Event.ADDED_OBJECT, this, subscriptionRegistrar);
+        }
+        
+        @Override
+        public void eventOccurred(Object key, Publisher publisher, Object hint) {
+            if (key == Session.Event.ADDED_OBJECT && (hint instanceof User)) {
+                onSelectUser((User) hint);
+            } else if (key == Session.Event.ADDED_OBJECT && (hint instanceof Course)) {
+                
+            }
+        }
+        
+        private void reloadUser() {
+            userAccountView.loadUser(getSession());
+        }
+        
+        private void onSelectUser(User user) {
+            // Problem selected: enable/disable buttons appropriately
+            userManagementButtons[ButtonPanelAction.EDIT.ordinal()].setEnabled(true);
+            userManagementButtons[ButtonPanelAction.VIEW_PROGRESS.ordinal()].setEnabled(true);
+        }
+        
+        private void handleEditUser(ClickEvent event) {
+            GWT.log("handle edit user");
+            final User chosen = getSession().get(User.class);
+            final Course course = getSession().get(Course.class);
+            //TODO get the course type?
+            //TODO wtf is the in the user record and how does it get there?
+            CourseRegistrationType type=null;
+            Widget w = (Widget)event.getSource();
+            EditUserPopupPanel pop = new EditUserPopupPanel(w, 
+                    chosen, 
+                    course,
+                    type);
+            pop.center();
+            pop.setGlassEnabled(true);
+            pop.show();
+        }
+        
+        private void handleUserProgress(ClickEvent event) {
+            GWT.log("handle user progress");
+            final User chosen = getSession().get(User.class);
+            final Course course = getSession().get(Course.class);
+            
+            GWT.log("handling user "+chosen.getUsername());
+            //TODO get the course type?
+            //TODO wtf is the in the user record and how does it get there?
+            CourseRegistrationType type=null;
+            Widget w = (Widget)event.getSource();
+            UserProgressPopupPanel pop = new UserProgressPopupPanel(w, 
+                    chosen, 
+                    course,
+                    type,
+                    getSession());
+            pop.center();
+            pop.setGlassEnabled(true);
+            pop.show();
+        }
+    }
+    
+    private UI ui;
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.client.page.CloudCoderPage#createWidget()
+     */
+    @Override
+    public void createWidget() {
+        ui = new UI();
+    }
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.client.page.CloudCoderPage#activate()
+     */
+    @Override
+    public void activate() {
+        ui.activate(getSession(), getSubscriptionRegistrar());
+    }
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.client.page.CloudCoderPage#deactivate()
+     */
+    @Override
+    public void deactivate() {
+        getSubscriptionRegistrar().cancelAllSubscriptions();
+    }
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.client.page.CloudCoderPage#getWidget()
+     */
+    @Override
+    public IsWidget getWidget() {
+        return ui;
+    }
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.client.page.CloudCoderPage#isActivity()
+     */
+    @Override
+    public boolean isActivity() {
+        return true;
+    }
+}
---
+++ CloudCoder/src/org/cloudcoder/app/client/view/UserAccountView.java
@@ -0,0 +1,160 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.client.view;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.cloudcoder.app.client.model.Session;
+import org.cloudcoder.app.client.model.StatusMessage;
+import org.cloudcoder.app.client.page.SessionObserver;
+import org.cloudcoder.app.client.rpc.RPC;
+import org.cloudcoder.app.shared.model.Course;
+import org.cloudcoder.app.shared.model.User;
+import org.cloudcoder.app.shared.util.Publisher;
+import org.cloudcoder.app.shared.util.Subscriber;
+import org.cloudcoder.app.shared.util.SubscriptionRegistrar;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.ResizeComposite;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SelectionChangeEvent.Handler;
+import com.google.gwt.view.client.SingleSelectionModel;
+
+/**
+ * @author jaimespacco
+ * @author Andrei Papancea
+ */
+public class UserAccountView extends ResizeComposite implements Subscriber, SessionObserver
+{
+    private DataGrid<User> grid;
+    private Session session;
+    
+    /**
+     * Constructor.
+     */
+    public UserAccountView() {
+        grid = new DataGrid<User>();
+        grid.addColumn(new TextColumn<User>() {
+            @Override
+            public String getValue(User user) {
+                return user.getUsername();
+            }
+        }, "Username");
+        grid.addColumn(new TextColumn<User>() {
+            @Override
+            public String getValue(User user) {
+                return user.getFirstname();
+            }
+        }, "Firstname");
+        grid.addColumn(new TextColumn<User>() {
+            @Override
+            public String getValue(User user) {
+                return user.getLastname();
+            }
+        }, "Lastname");
+        grid.addColumn(new TextColumn<User>() {
+            @Override
+            public String getValue(User user) {
+                return user.getEmail();
+            }
+        }, "Email");
+        
+        initWidget(grid);
+    }
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.client.page.SessionObserver#activate(org.cloudcoder.app.client.model.Session, org.cloudcoder.app.shared.util.SubscriptionRegistrar)
+     */
+    @Override
+    public void activate(final Session session, SubscriptionRegistrar subscriptionRegistrar)
+    {
+        this.session = session;
+        session.subscribe(Session.Event.USER_ACCOUNT, this, subscriptionRegistrar);
+        
+        // Set selection model.
+        // When a User record is selected, it will be added to the Session.
+        final SingleSelectionModel<User> selectionModel = new SingleSelectionModel<User>();
+        selectionModel.addSelectionChangeHandler(new Handler() {
+            @Override
+            public void onSelectionChange(SelectionChangeEvent event) {
+                User selected = selectionModel.getSelectedObject();
+                if (selected != null) {
+                    session.add(selected);
+                }
+            }
+        });
+        grid.setSelectionModel(selectionModel);
+        
+        // If the session contains a list of Users, display them.
+        // Otherwise, initiate loading of users for this course.
+        User[] userList = session.get(User[].class);
+        	
+        //ProblemAndSubmissionReceipt[] problemAndSubmissionReceiptList = session.get(ProblemAndSubmissionReceipt[].class);
+        if (userList != null) {
+            displayUser(userList);
+        } else {
+            loadUser(session);
+        }
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.cloudcoder.app.shared.util.Subscriber#eventOccurred(java.lang.Object, org.cloudcoder.app.shared.util.Publisher, java.lang.Object)
+     */
+    @Override
+    public void eventOccurred(Object key, Publisher publisher, Object hint) {
+        if (key == Session.Event.USER_ACCOUNT && (hint instanceof Course)) {
+            // load all the users for the current course
+            loadUser(session);
+        }
+    }
+    
+    public void loadUser(final Session session) {
+        Course course=session.get(Course.class);
+        int courseId=course.getId();
+        RPC.usersService.getUsers(courseId, new AsyncCallback<User[]>() {
+            @Override
+            public void onSuccess(User[] result) {
+                displayUser(result);
+            }
+            
+            @Override
+            public void onFailure(Throwable caught) {
+                session.add(new StatusMessage(StatusMessage.Category.ERROR, "Could not load user account"));
+            }
+        });
+    }
+
+    protected void displayUser(User[] result) {
+    	User user = session.get(User.class);
+    	for(User u:result){
+    		if(u.getUsername().equals(user.getUsername())){
+    			grid.setRowCount(1);
+    			List<User> list = new LinkedList<User>();
+    			list.add(u);
+    			grid.setRowData(list);
+    			return;
+    		}
+    	}
+    }
+}
