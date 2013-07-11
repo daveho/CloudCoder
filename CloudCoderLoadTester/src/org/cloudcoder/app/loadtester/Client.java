21efdb03cc2b7fdd2c1c6660ec3f61f82056b884
David Hovemeyer <david.hovemeyer@gmail.com>
2013-07-11 15:36:55 -0400

    added Client class as general front-end for client session
    Removed RPC class.

--- CloudCoderLoadTester/src/org/cloudcoder/app/loadtester/Client.java
+++ CloudCoderLoadTester/src/org/cloudcoder/app/loadtester/Client.java
@@ -1,13 +1,48 @@
 package org.cloudcoder.app.loadtester;
 
+import java.util.HashMap;
+
+import org.cloudcoder.app.client.rpc.LoginService;
 import org.cloudcoder.app.shared.model.User;
 
+import com.gdevelop.gwt.syncrpc.SyncProxy;
+
 public class Client {
-	public static void main(String[] args) {
-		User user = RPC.loginSvc.login("dhovemey", "muffin");
-		
-		System.out.println(user != null ? "Logged in!" : "Could not log in");
-		
-		System.out.println("User email is " + user.getEmail());
+//	public static void main(String[] args) {
+//		User user = RPC.loginSvc.login("dhovemey", "muffin");
+//		
+//		System.out.println(user != null ? "Logged in!" : "Could not log in");
+//		
+//		System.out.println("User email is " + user.getEmail());
+//	}
+	
+	private String host;
+	private int port;
+	private String contextPath;
+	private HashMap<Class<?>, Object> serviceMap;
+	private User user;
+	
+	public Client(String host, int port, String contextPath) {
+		this.host = host;
+		this.port = port;
+		this.contextPath = contextPath;
+		this.serviceMap = new HashMap<Class<?>, Object>();
+	}
+	
+	private<E> E getService(Class<E> cls, String relativePath) {
+		Object obj = serviceMap.get(cls);
+		if (obj == null) {
+			obj = SyncProxy.newProxyInstance(cls, "http://" + host + ":" + port + "/" + contextPath + "/", relativePath);
+			serviceMap.put(cls, obj);
+		}
+		return cls.cast(obj);
+	}
+	
+	public void login(String username, String password) {
+		LoginService loginSvc = getService(LoginService.class, "login");
+		this.user = loginSvc.login(username, password);
+		if (user == null) {
+			throw new IllegalArgumentException("Could not log in");
+		}
 	}
 }
---
+++ CloudCoderLoadTester/src/org/cloudcoder/app/loadtester/Main.java
@@ -0,0 +1,9 @@
+package org.cloudcoder.app.loadtester;
+
+public class Main {
+	public static void main(String[] args) {
+		Client client = new Client("localhost", 8888, "cloudcoder");
+		client.login("dhovemey", "muffin");
+		System.out.println("Successful login!");
+	}
+}
