1c47815f3e165ee7ad0fcac70212fc5e63ce744e
David Hovemeyer <david.hovemeyer@gmail.com>
2013-07-30 12:45:30 -0400

    add PasswordUtil facade for password hashing; reduce bcrypt log2 rounds
    All of the sites were BCrypt methods were being called
    have been replaced by calls to PasswordUtil methods.
    BCrypt has been made package-protected (to avoid direct calls.)
    
    Reducing the bcrypt log2 hash rounds from 12 to 10 (where 10
    is the default) significantly reduces CPU load on login.

---
+++ CloudCoderModelClassesPersistence/src/org/cloudcoder/app/server/persist/PasswordUtil.java
@@ -0,0 +1,37 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.server.persist;
+
+/**
+ * Password utility methods.
+ * Call these methods in preference to directly calling the
+ * underlying BCrypt methods: that way, we can ensure that
+ * BCrypt is used consistently (e.g., salt generation,
+ * how many rounds of hashing to require, etc.) 
+ * 
+ * @author David Hovemeyer
+ */
+public class PasswordUtil {
+	public static String hashPassword(String plaintext) {
+		return BCrypt.hashpw(plaintext, BCrypt.gensalt());
+	}
+	
+	public static boolean matches(String plaintext, String hashed) {
+		return BCrypt.checkpw(plaintext, hashed);
+	}
+}
