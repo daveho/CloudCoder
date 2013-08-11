d78278164d4597e15d618194c96bc7f03b200ef7
David Hovemeyer <david.hovemeyer@gmail.com>
2013-08-11 11:15:21 -0400

    add header comment, javadoc

--- CloudCoderBuilderWebService/src/org/cloudcoder/builderwebservice/BuilderWebServiceJettyDaemonConfig.java
+++ CloudCoderBuilderWebService/src/org/cloudcoder/builderwebservice/BuilderWebServiceJettyDaemonConfig.java
@@ -1,7 +1,36 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2013, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2013, David H. Hovemeyer <david.hovemeyer@gmail.com>
+// Copyright (C) 2013, York College of Pennsylvania
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package org.cloudcoder.builderwebservice;
 
 import org.cloudcoder.jetty.JettyDaemonConfig;
 
+/**
+ * Extension of {@link JettyDaemonConfig} specifying configuration information
+ * for the builder web service.
+ * 
+ * @author David Hovemeyer
+ */
 public interface BuilderWebServiceJettyDaemonConfig extends JettyDaemonConfig {
+	/**
+	 * Get the context path.
+	 * 
+	 * @return the context path
+	 */
 	public String getContextPath();
 }
