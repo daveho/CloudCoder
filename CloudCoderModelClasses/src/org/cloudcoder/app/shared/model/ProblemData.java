f5a07748f9f2e93680fdfdd42d991dd71cccba50
David Hovemeyer <david.hovemeyer@gmail.com>
2013-08-16 14:23:35 -0400

    updated copyright header

--- CloudCoderModelClasses/src/org/cloudcoder/app/shared/model/ProblemData.java
+++ CloudCoderModelClasses/src/org/cloudcoder/app/shared/model/ProblemData.java
@@ -1,6 +1,7 @@
 // CloudCoder - a web-based pedagogical programming environment
-// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
-// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+// Copyright (C) 2011-2013, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2013, David H. Hovemeyer <david.hovemeyer@gmail.com>
+// Copyright (C) 2013, York College of Pennsylvania
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
