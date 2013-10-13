ce24b761f74a47f01cd04d5184d5e07b469d4b04
David Hovemeyer <david.hovemeyer@gmail.com>
2013-10-13 15:36:15 -0400

    added copyright header

--- CloudCoderBuilder2/src/org/cloudcoder/builder2/pythonfunction/AddPythonFunctionScaffoldingBuildStep.java
+++ CloudCoderBuilder2/src/org/cloudcoder/builder2/pythonfunction/AddPythonFunctionScaffoldingBuildStep.java
@@ -1,8 +1,27 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+// Copyright (C) 2013, York College of Pennsylvania
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package org.cloudcoder.builder2.pythonfunction;
 
 import java.util.Properties;
 
 import org.cloudcoder.app.shared.model.Problem;
+import org.cloudcoder.app.shared.model.ProblemType;
 import org.cloudcoder.app.shared.model.TestCase;
 import org.cloudcoder.builder2.model.BuilderSubmission;
 import org.cloudcoder.builder2.model.IBuildStep;
@@ -10,6 +29,12 @@ import org.cloudcoder.builder2.model.InternalBuilderException;
 import org.cloudcoder.builder2.model.ProgramSource;
 import org.cloudcoder.builder2.util.StringUtil;
 
+/**
+ * An {@link IBuildStep} class to add test scaffolding to a {@link ProblemType#PYTHON_FUNCTION}
+ * submission.
+ * 
+ * @author David Hovemeyer
+ */
 public class AddPythonFunctionScaffoldingBuildStep implements IBuildStep {
 
 	@Override
