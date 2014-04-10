d8da0bfe8bfb58eff8a16c52f2bfa8d51b72fc1b
David Hovemeyer <david.hovemeyer@gmail.com>
2014-04-10 15:29:52 -0400

    report when instance statuses change
    Also report unhealthy instances if the last report was
    sufficiently long in the past (currently defaults to 2 hours.)
    
    Things seem to be in pretty good shape. Next step is
    packaging it for deployment.

--- CloudCoderHealthMonitor/src/org/cloudcoder/healthmonitor/HealthMonitor.java
+++ CloudCoderHealthMonitor/src/org/cloudcoder/healthmonitor/HealthMonitor.java
@@ -17,8 +17,12 @@
 
 package org.cloudcoder.healthmonitor;
 
-import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -54,11 +58,17 @@ public class HealthMonitor implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(HealthMonitorConfig.class);
 	
 	// Check instances every 5 minutes
-	private static final int CHECK_INTERVAL_SEC = Integer.getInteger("cloudcoder.healthmonitor.checkIntervalSec", 5*60);
+	private static final int CHECK_INTERVAL_SEC =
+			Integer.getInteger("cloudcoder.healthmonitor.checkIntervalSec", 5*60);
 
 	// If we see a submission queue size spike greater than this threshold
 	// then report it.  TODO: make this configurable.
 	private static final int SUBMISSION_QUEUE_DANGER_THRESHOLD = 15;
+	
+	// If an instance has been offline for more than this many seconds,
+	// send an email report.  TODO: make this configurable.
+	private static final long OFFLINE_INSTANCE_RESEND_INTERVAL_SEC =
+			Integer.getInteger("cloudcoder.healthmonitor.offlineInstanceResendIntervalSec", 2*60*60);
 	
 	private Object lock;
 	private HealthMonitorConfig config;
@@ -93,8 +103,66 @@ public class HealthMonitor implements Runnable {
 		// Caller is responsible for interrupting the thread
 	}
 	
+	/**
+	 * Object representing the last known status, last report timestamp,
+	 * and unhealthyTimestamp for an instance.
+	 */
+	private static class Info {
+		final Status status;
+		final long lastReportTimestamp;
+		final long unhealthyTimestamp;  // when the instance was found to be unhealthy
+		
+		Info(Status status, long lastReportTimestamp, long unhealthyTimestamp) {
+			this.status = status;
+			this.lastReportTimestamp = lastReportTimestamp;
+			this.unhealthyTimestamp = unhealthyTimestamp;
+		}
+	}
+	
+	/**
+	 * Information about an instance including the {@link Entry}
+	 * representing its current status and the {@link Info} representing
+	 * its last known status and report time.
+	 */
+	private static class ReportItem {
+		final Entry entry;
+		final Info info;
+		
+		public ReportItem(Entry entry, Info info) {
+			this.entry = entry;
+			this.info = info;
+		}
+		
+		boolean statusChange() {
+			return entry.status != info.status;
+		}
+		
+		boolean goodNews() {
+			return statusChange() && entry.status == Status.HEALTHY;
+		}
+		
+		boolean badNews() {
+			if (statusChange() && info.status == Status.HEALTHY) {
+				return true;
+			}
+			
+			// Instances that have been offline a long time are bad news,
+			// even if there is no status change.
+			if (entry.status != Status.HEALTHY) {
+				long timeElapsedSinceLastReport = System.currentTimeMillis() - info.lastReportTimestamp;
+				if (timeElapsedSinceLastReport > OFFLINE_INSTANCE_RESEND_INTERVAL_SEC*1000L) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+	}
+	
 	@Override
 	public void run() {
+		Map<String, Info> infoMap = new HashMap<String, Info>();
+		
 		while (!shutdown) {
 			try {
 				Thread.sleep(CHECK_INTERVAL_SEC * 1000);
@@ -112,24 +180,21 @@ public class HealthMonitor implements Runnable {
 					report.addEntry(checkInstance(instance));
 				}
 				
-				if (report.hasUnhealthyStatus()) {
-					logger.error("Unhealthy status detected!");
-					for (Entry entry : report.getEntryList()) {
-						logger.error("Instance {}, status={}", entry.instance, entry.status);
-					}
-					
-					// TODO: send email!
-					//Session session = createMailSession(config);
-					sendReportEmail(report, config);
-				} else {
-					logger.debug("All instances are healthy");
-				}
+				// Update statuses and send email if appropriate
+				update(report, infoMap);
 			} catch (InterruptedException e) {
 				logger.info("HealthMonitor interrupted (shutdown requested?)");
 			}
 		}
 	}
 
+	/**
+	 * Check a webapp instance to see whether or not it is healthy,
+	 * creating an {@link Entry}.
+	 *   
+	 * @param instance the webapp instance to check
+	 * @return the {@link Entry} describing the health of the instance
+	 */
 	private Entry checkInstance(String instance) {
 		logger.debug("Checking instance {}", instance);
 		
@@ -176,41 +241,138 @@ public class HealthMonitor implements Runnable {
 		return result;
 	}
 
-	private void sendReportEmail(HealthMonitorReport report, HealthMonitorConfig config) {
-		try {
-			Session session = createMailSession(config);
-	
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(config.getReportEmailAddress()));
-			message.addRecipient(RecipientType.TO, new InternetAddress(config.getReportEmailAddress()));
-			message.setSubject("CloudCoder health monitor failure report");
-			
-			StringBuilder body = new StringBuilder();
-			body.append("<h1>CloudCoder health monitor failure report</h1>");
-			body.append("<p>One or more CloudCoder webapp instances are unhealthy:</p>");
-			body.append("<ul>");
-			for (Entry entry : report.getEntryList()) {
-				if (entry.status != Status.HEALTHY) {
-					body.append("<li>");
-					body.append(entry.instance);
-					body.append(": ");
-					body.append(entry.status);
-					body.append(" at ");
-					SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
-					body.append(fmt.format(new Date(entry.timestamp)));
-					body.append("</li>");
-				}
+	/**
+	 * Based on new information (the {@link HealthMonitorReport}) and
+	 * current information (the info map, which keeps track of the
+	 * last known status for each monitored instance), see whether
+	 * any instance statuses have changed, and send email as appropriate.
+	 * 
+	 * @param report   the {@link HealthMonitorReport} representing current statuses
+	 * @param infoMap  the map of instances to previously-known statuses
+	 */
+	private void update(HealthMonitorReport report, Map<String, Info> infoMap) {
+		// Generate ReportItems
+		List<ReportItem> reportItems = new ArrayList<ReportItem>();
+		for (Entry entry : report.getEntryList()) {
+			Info info = infoMap.get(entry.instance);
+			if (info == null) {
+				// no previous status: pretend the instance was fine
+				info = new Info(Status.HEALTHY, 0L, -1L);
 			}
-			
-			message.setContent(body.toString(), "text/html");
-			
-			Transport.send(message);
-		} catch (Exception e) {
-			// This is bad
-			logger.error("Error sending report email!", e);
+			reportItems.add(new ReportItem(entry, info));
+		}
+		
+		// See if any statuses have changed
+		boolean goodNews = false;
+		boolean badNews = false;
+		for (ReportItem item : reportItems) {
+			if (item.goodNews()) {
+				goodNews = true;
+			}
+			if (item.badNews()) {
+				badNews = true;
+			}
+		}
+		
+		if (goodNews || badNews) {
+			// Generate email report
+			try {
+				Session session = createMailSession(config);
+				
+				MimeMessage message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(config.getReportEmailAddress()));
+				message.addRecipient(RecipientType.TO, new InternetAddress(config.getReportEmailAddress()));
+				message.setSubject("CloudCoder health monitor report");
+				
+				StringBuilder body = new StringBuilder();
+				body.append("<h1>CloudCoder health monitor report</h1>\n");
+				
+				if (badNews) {
+					body.append("<h2>Unhealthy instances</h2>\n");
+					body.append("<ul>\n");
+					for (ReportItem item : reportItems) {
+						if (item.badNews()) {
+							appendReportItem(body, item, infoMap);
+						}
+					}
+					body.append("</ul>\n");
+				}
+				
+				if (goodNews) {
+					body.append("<h2>Healthy instances (back on line)</h2>\n");
+					body.append("<ul>\n");
+					for (ReportItem item : reportItems) {
+						if (item.goodNews()) {
+							appendReportItem(body, item, infoMap);
+						}
+					}
+					body.append("</ul>\n");
+				}
+				
+				message.setContent(body.toString(), "text/html");
+				
+				Transport.send(message);
+			} catch (Exception e) {
+				// This is bad
+				logger.error("Could not send report email!", e);
+			}
 		}
 	}
 
+	/**
+	 * Append a report item to a list in the body of a report email.
+	 * 
+	 * @param body     StringBuilder being used to build the body of the report email
+	 * @param item     the report item to append
+	 * @param infoMap  map of instances to Info objects recording current status;
+	 *                 the lastReportTimestamp for the instance will be updated
+	 */
+	private void appendReportItem(StringBuilder body, ReportItem item, Map<String, Info> infoMap) {
+		body.append("<li>");
+		body.append(item.entry.instance);
+		body.append(": ");
+		body.append(item.entry.status);
+		body.append(" at ");
+		SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
+		body.append(fmt.format(new Date(item.entry.timestamp)));
+
+		long now = System.currentTimeMillis();
+		
+		// Determine updated unhealthyTimestamp
+		long unhealthyTimestamp;
+		if (item.entry.status == Status.HEALTHY) {
+			// Instance is healthy, reset unhealthyTimestamp
+			unhealthyTimestamp = -1L;
+		} else {
+			if (item.info.status == Status.HEALTHY) {
+				// Instance just went offline
+				unhealthyTimestamp = now;
+			} else {
+				// Instance remains offline
+				unhealthyTimestamp = item.info.unhealthyTimestamp;
+			}
+		}
+		
+		if (item.badNews() && item.info.status != Status.HEALTHY) {
+			body.append(" (unhealthy since");
+			body.append(fmt.format(new Date(unhealthyTimestamp)));
+			body.append(")");
+		}
+		
+		body.append("</li>\n");
+		
+		// Since a report has been generated, update the Info for the instance
+		Info updatedInfo = new Info(item.entry.status, now, unhealthyTimestamp);
+		infoMap.put(item.entry.instance, updatedInfo);
+	}
+
+	/**
+	 * Create a mail Session based on information in the
+	 * given {@link HealthMonitorConfig}.
+	 * 
+	 * @param config the {@link HealthMonitorConfig}
+	 * @return the mail Session
+	 */
 	private Session createMailSession(HealthMonitorConfig config) {
 		final PasswordAuthentication passwordAuthentication =
 				new PasswordAuthentication(config.getSmtpUsername(), config.getSmtpPassword());
