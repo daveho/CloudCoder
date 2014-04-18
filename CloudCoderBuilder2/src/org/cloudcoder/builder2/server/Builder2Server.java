dbad8444a8a30226ace86219b22bcf9d41218765
David Hovemeyer <david.hovemeyer@gmail.com>
2014-04-18 10:19:52 -0400

    update copyright year, make socket field volatile

--- CloudCoderBuilder2/src/org/cloudcoder/builder2/server/Builder2Server.java
+++ CloudCoderBuilder2/src/org/cloudcoder/builder2/server/Builder2Server.java
@@ -1,6 +1,6 @@
 // CloudCoder - a web-based pedagogical programming environment
-// Copyright (C) 2011-2012, Jaime Spacco <jspacco@knox.edu>
-// Copyright (C) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+// Copyright (C) 2011-2014, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2014, David H. Hovemeyer <david.hovemeyer@gmail.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -95,7 +95,7 @@ public class Builder2Server implements Runnable {
     private NoConnectTimer noConnectTimer;
     private WebappSocketFactory webappSocketFactory;
     private Builder2 builder2;
-    private Socket socket;
+    private volatile Socket socket;
     private ObjectInputStream in;
     private ObjectOutputStream out;
     private Thread watchdogThread;
