64839bcdd20cc5a218876004b4a684a834b30d42
David Hovemeyer <david.hovemeyer@gmail.com>
2014-04-29 16:31:03 -0400

    CloudCoderTools project is no longer used/needed

--- CloudCoderTools/src/org/cloudcoder/importer/Now.java
+++
@@ -1,9 +0,0 @@
-package org.cloudcoder.importer;
-
-public class Now {
-	public static void main(String[] args) {
-		long now = System.currentTimeMillis();
-		System.out.println("Now: " + now);
-		System.out.println("This time tomorrow: " + (now + (24L * 60L * 60L * 1000L)));
-	}
-}
