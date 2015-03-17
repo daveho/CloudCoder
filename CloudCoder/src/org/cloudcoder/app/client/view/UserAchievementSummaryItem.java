16aa442374fec56ab122690b02c64de5a651c9c7
Shane Bonner <sbonner1@ycp.edu>
2015-03-17 16:46:14 -0400

    last commit did not include userAchievementSummaryItem or userAchievementSummaryView

---
+++ CloudCoder/src/org/cloudcoder/app/client/view/UserAchievementSummaryItem.java
@@ -0,0 +1,141 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2014, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2014, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.client.view;
+
+import org.cloudcoder.app.shared.model.ICallback;
+import org.cloudcoder.app.shared.model.ProblemAndSubmissionReceipt;
+import org.cloudcoder.app.shared.model.SubmissionStatus;
+import org.cloudcoder.app.shared.model.UserAchievementAndAchievement;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ErrorEvent;
+import com.google.gwt.event.dom.client.ErrorHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * @author shanembonner
+ *
+ */
+
+public class UserAchievementSummaryItem extends Composite { 
+//	private HTML html;
+	private UserAchievementAndAchievement userAchievementAndAchievement;
+	private ICallback<UserAchievementSummaryItem> clickHandler;
+	private Image image;
+	private Label label;
+	
+	public UserAchievementSummaryItem(){
+//		this.html = new HTML("<span></span>");
+//		html.addClickHandler(new ClickHandler() {
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				onClickEvent();
+//			}
+//		});
+//		configureStyle(); // Just set a default style
+//		initWidget(html);
+		
+		final Image image = new Image();
+		// TODO: create and configure Image
+
+
+
+		ErrorHandler handler = new ErrorHandler(){
+			public void onError(ErrorEvent event){
+				label.setText("An error occurred while loading.");
+			}
+		};
+		
+		image.addErrorHandler(handler);
+		image.setUrl("what's the URL?");
+		initWidget(image);
+	}
+
+	
+	protected void onClickEvent() {
+		if (clickHandler != null) {
+			clickHandler.call(this);
+		}
+	}
+	
+	/**
+	 * set this item's UserAchievementAndAchievement
+	 * @param item
+	 */
+	public void setProblemAndSubmissionReceipt(UserAchievementAndAchievement item) {
+		this.userAchievementAndAchievement = item;
+		configureStyle();
+		//setTooltip(item.getAchievement().); FIXME: find the correct title for the achievement
+	}
+	
+	/**
+	 * get this item's userAchievementAndAchievement item
+	 * @return
+	 */
+	public UserAchievementAndAchievement getUserAchievementAndAchievement(){
+		return userAchievementAndAchievement;
+	}
+	
+	/**
+	 * Set a callback to be invoked when the user clicks on this
+	 * item.  The {@link AchievementSummaryView} will use this to
+	 * handle item clicks.
+	 * 
+	 * @param clickHandler the click handler to set
+	 */
+	public void setClickHandler(ICallback<UserAchievementSummaryItem> clickHandler) {
+		this.clickHandler = clickHandler;
+	}
+	
+	/**
+	 * If the userAchievement field in the userAchievementAndAchievement object is null,
+	 * the user has not earned that achievement. If it exists, the user has earned it.
+	 * @return
+	 */
+	public boolean getStatus(){
+		if(userAchievementAndAchievement.getUserAchievement() == null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	private void configureStyle() {
+		
+		this.setStyleName("cc-achievementSummaryItem"); //WHY DON'T I NEED THE HTML FIELD?
+		
+		//find out if the user has earned the achievement
+		Boolean status = getStatus();
+		
+		//if the achievement has not been earned, the item will appear transparent.
+		if(status == false){
+			this.setStyleName("cc-achievementNotEarned");
+		}else{
+			this.setStyleName("cc-achievementEarned");
+		}
+		
+	}
+	
+	private void setTooltip(String tooltip) {
+		image.getElement().setAttribute("title", tooltip);
+	}
+}
---
+++ CloudCoder/src/org/cloudcoder/app/client/view/UserAchievementSummaryView.java
@@ -0,0 +1,115 @@
+// CloudCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011-2014, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011-2014, David H. Hovemeyer <david.hovemeyer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.cloudcoder.app.client.view;
+
+import org.cloudcoder.app.client.model.Session;
+import org.cloudcoder.app.client.page.SessionObserver;
+import org.cloudcoder.app.shared.model.ICallback;
+import org.cloudcoder.app.shared.model.ProblemAndSubmissionReceipt;
+import org.cloudcoder.app.shared.util.Publisher;
+import org.cloudcoder.app.shared.util.Subscriber;
+import org.cloudcoder.app.shared.util.SubscriptionRegistrar;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+
+/**
+ * @author shanembonner
+ * Achievement summary view: provides a quick visual summary of
+ * achievements that the u.
+ *
+ */
+public class UserAchievementSummaryView extends Composite implements Subscriber, SessionObserver{
+	private Session session;
+	private FlowPanel flowPanel;
+	private ICallback<UserAchievementSummaryItem> clickHandler;
+
+	/**
+	 * Constructor.
+	 */
+	public UserAchievementSummaryView() {
+		// This widget is basically a div containing AchievementSummaryItems
+		this.flowPanel = new FlowPanel();
+		//flowPanel.setStyleName("cc-exerciseSummary", true);
+
+		// Allow it to scroll
+		ScrollPanel wrap = new ScrollPanel();
+		wrap.add(flowPanel);
+		
+		initWidget(wrap);
+		
+		// Create a callback to handle item clicks
+		this.clickHandler = new ICallback<UserAchievementSummaryItem>() {
+			@Override
+			public void call(UserAchievementSummaryItem value) {
+				handleItemClick(value);
+			}
+		};
+	}
+
+	protected void handleItemClick(UserAchievementSummaryItem value) {
+		// Add the Problem to the session (to select the problem)
+		//ProblemAndSubmissionReceipt p = value.getProblemAndSubmissionReceipt();
+		//session.add(p.getProblem());
+	}
+
+	@Override
+	public void activate(final Session session, SubscriptionRegistrar subscriptionRegistrar)
+	{
+		this.session = session;
+		
+		session.subscribe(Session.Event.ADDED_OBJECT, this, subscriptionRegistrar);
+		ProblemAndSubmissionReceipt[] problemAndSubmissionReceipts = session.get(ProblemAndSubmissionReceipt[].class);
+		if (problemAndSubmissionReceipts != null) {
+			//loadData(problemAndSubmissionReceipts);
+		}
+	}
+
+	@Override
+	public void eventOccurred(Object key, Publisher publisher, Object hint) {
+		//if (key == Session.Event.ADDED_OBJECT && hint instanceof ProblemAndSubmissionReceipt[]) {
+		//	loadData((ProblemAndSubmissionReceipt[]) hint);
+		//}
+	}
+
+	/**
+	 * @param problemAndSubmissionReceipts
+	 */
+/*	private void loadData(ProblemAndSubmissionReceipt[] problemAndSubmissionReceipts) {
+		// clear current data, load new data
+		flowPanel.clear();
+		
+		// Sort by due date
+		ProblemAndSubmissionReceipt[] list = new ProblemAndSubmissionReceipt[problemAndSubmissionReceipts.length]; //problemAndSubmissionReceipts.clone();
+		System.arraycopy(problemAndSubmissionReceipts, 0, list, 0, problemAndSubmissionReceipts.length);
+		ViewUtil.sortProblemsByDueDate(list);
+		
+		//loop through the problem and submission receipts,
+		//create exerciseSummaryItem for each, and set each box's status (completed, failed, etc)
+		
+		for (ProblemAndSubmissionReceipt item : list){
+			// Create ExerciseSummaryItem
+			ExerciseSummaryItem summaryItem = new ExerciseSummaryItem();
+			summaryItem.setProblemAndSubmissionReceipt(item);
+			summaryItem.setClickHandler(clickHandler);
+			flowPanel.add(summaryItem);
+		}
+	}
+	*/
+}
