b90094209fb528849f7731d886fc3a996a3d7b52
David Hovemeyer <david.hovemeyer@gmail.com>
2016-02-22 16:37:43 -0500

    create VPC for CloudCoder if necessary

--- CloudCoderInstallationWizard/src/org/cloudcoder/app/wizard/exec/CloudService.java
+++ CloudCoderInstallationWizard/src/org/cloudcoder/app/wizard/exec/CloudService.java
@@ -1,5 +1,6 @@
 package org.cloudcoder.app.wizard.exec;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +12,9 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.CreateVpcRequest;
+import com.amazonaws.services.ec2.model.CreateVpcResult;
 import com.amazonaws.services.ec2.model.DescribeVpcsResult;
 import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.ec2.model.Vpc;
@@ -49,15 +53,36 @@ public class CloudService {
 	public void createVpc() throws ExecException {
 		try {
 			// See if there is an existing VPC
-			//boolean foundCloudcoderVPC = false;
+			Vpc cloudcoderVpc = null;
 			DescribeVpcsResult vpcs = client.describeVpcs();
 			for (Vpc vpc : vpcs.getVpcs()) {
 				List<Tag> tags = vpc.getTags();
-				System.out.println("VPC id: " + vpc.getVpcId());
+				//System.out.println("VPC id: " + vpc.getVpcId());
 				for (Tag t : tags) {
-					System.out.printf("  Tag: key=%s, value=%s\n", t.getKey(), t.getValue());
+					//System.out.printf("  Tag: key=%s, value=%s\n", t.getKey(), t.getValue());
+					if (t.getKey().equals("Name") && t.getValue().equals(CLOUDCODER_VPC_NAME)) {
+						cloudcoderVpc = vpc;
+						break;
+					}
 				}
 			}
+			
+			if (cloudcoderVpc != null) {
+				System.out.println("Found " + CLOUDCODER_VPC_NAME + ", id=" + cloudcoderVpc.getVpcId());
+				return;
+			}
+			
+			// Create a VPC
+			CreateVpcRequest req = new CreateVpcRequest("10.0.0.0/24");
+			CreateVpcResult result = client.createVpc(req);
+			cloudcoderVpc = result.getVpc();
+			
+			// Tag it with the correct name
+			CreateTagsRequest tagReq = new CreateTagsRequest();
+			tagReq.setTags(Arrays.asList(new Tag("Name", CLOUDCODER_VPC_NAME)));
+			tagReq.setResources(Arrays.asList(cloudcoderVpc.getVpcId()));
+			client.createTags(tagReq);
+			System.out.printf("Tagged VPC %s with Name=%s\n", cloudcoderVpc.getVpcId(), CLOUDCODER_VPC_NAME);
 		} catch (AmazonServiceException e) {
 			throw new ExecException("Failed to login to enumerate VPCs/create new VPC", e);
 		}
