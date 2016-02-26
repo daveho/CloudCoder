f74510bdafa0091245a6699d1d39b0170de14e63
David Hovemeyer <david.hovemeyer@gmail.com>
2016-02-26 10:17:05 -0500

    successfully fetched bootstrap script on webapp instance

--- CloudCoderInstallationWizard/src/org/cloudcoder/app/wizard/exec/Bootstrap.java
+++ CloudCoderInstallationWizard/src/org/cloudcoder/app/wizard/exec/Bootstrap.java
@@ -3,9 +3,15 @@ package org.cloudcoder.app.wizard.exec;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
@@ -14,14 +20,49 @@ import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
 
 public class Bootstrap {
-	public static final String DOWNLOAD_SITE = "https://s3.amazonaws.com/cloudcoder-binaries/";
-	public static final String BOOTSTRAP_SCRIPT = DOWNLOAD_SITE + "/bootstrap.pl";
+	private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
 	
-	private static class DrainStdout implements ICallbackWithIOException<Command> {
+	public static final String DOWNLOAD_SITE = "https://s3.amazonaws.com/cloudcoder-binaries/";
+	public static final String BOOTSTRAP_SCRIPT = DOWNLOAD_SITE + "bootstrap.pl";
+	
+	private static class Drain implements Runnable {
+		private InputStream is;
+		private StringWriter sink;
+		private Thread thread;
+		
+		public Drain(InputStream is) {
+			this.is = is;
+			this.sink = new StringWriter();
+		}
+		
+		public String getOutput() {
+			return sink.toString();
+		}
+		
 		@Override
-		public void call(Command value) throws IOException {
-			// FIXME: we ought to drain stderr as well
-			IOUtils.copy(value.getInputStream(), System.out);
+		public void run() {
+			InputStreamReader rdr = new InputStreamReader(is, Charset.forName("UTF-8"));
+			try {
+				IOUtils.copy(rdr, sink);
+			} catch (IOException e) {
+				logger.error("Error draining stream ", e);
+			} finally {
+				IOUtils.closeQuietly(rdr);
+				IOUtils.closeQuietly(sink);
+			}
+		}
+		
+		public void start() {
+			thread = new Thread(this);
+			thread.start();
+		}
+		
+		public void join() {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				logger.error("Interrupted while draining stream", e);
+			}
 		}
 	}
 	
@@ -33,38 +74,38 @@ public class Bootstrap {
 	
 	public void bootstrapWebappServer() throws ExecException {
 		try {
-			runCommand("wget " + BOOTSTRAP_SCRIPT, new DrainStdout());
+			SSHClient ssh = new SSHClient();
+			ssh.addHostKeyVerifier(new PromiscuousVerifier()); // FIXME: would be nice to have actual host key fingerprint
+			try {
+				ssh.connect(info.getWebappPublicIp());
+				KeyProvider keys = ssh.loadKeys(info.getPrivateKeyFile().getAbsolutePath());
+				ssh.authPublickey(info.getWebappServerUserName(), keys);
+				Session session = ssh.startSession();
+				try {
+					System.out.println("Downloading bootstrap script");
+					Command cmd = session.exec("wget " + BOOTSTRAP_SCRIPT);
+					//System.out.println(IOUtils.readFully(cmd.getInputStream()));
+					Drain out = new Drain(cmd.getInputStream());
+					Drain err = new Drain(cmd.getErrorStream());
+					out.start();
+					err.start();
+					out.join();
+					err.join();
+					cmd.join(10, TimeUnit.SECONDS);
+					System.out.println("Command exit code is " + cmd.getExitStatus());
+					System.out.println("Output:");
+					System.out.println(out.getOutput());
+					System.out.println("Error:");
+					System.out.println(err.getOutput());
+				} finally {
+					session.close();
+				}
+			} finally {
+				ssh.close();
+			}
+			
 		} catch (Exception e) {
 			throw new ExecException("Error bootstrapping CloudCoder on webapp instance", e);
-		}
-	}
-	
-	private void runCommand(final String command, final ICallbackWithIOException<Command> processCommand) throws IOException {
-		doSsh(new ICallbackWithIOException<Session>() {
-			@Override
-			public void call(Session value) throws IOException {
-				Command cmd = value.exec(command);
-				processCommand.call(cmd);
-			}
-		});
-	}
-
-	private void doSsh(ICallbackWithIOException<Session> callback) throws IOException {
-		SSHClient ssh = new SSHClient();
-		ssh.addHostKeyVerifier(new PromiscuousVerifier()); // FIXME: would be nice to have actual host key fingerprint
-		try {
-			ssh.connect(info.getWebappPublicIp());
-			KeyProvider keys = ssh.loadKeys(info.getPrivateKeyFile().getAbsolutePath());
-			ssh.authPublickey(info.getWebappServerUserName(), keys);
-			Session session = ssh.startSession();
-			try {
-//				System.out.println("We're connected?");
-				callback.call(session);
-			} finally {
-				session.close();
-			}
-		} finally {
-			ssh.close();
 		}
 	}
 	
