19df2e54a4ded1060bcb13261d60edcae70d13ec
David Hovemeyer <david.hovemeyer@gmail.com>
2019-08-12 13:39:39 -0400

    write compiler diagnostics to CSV files
    This should allow a failed process to be restarted if necessary.
    We will need to incorporate a "merge" utility to merge collected
    compiler diagnostics into the main event table.

--- CloudCoderDataAnalysis/src/org/cloudcoder/progsnap2/GatherCompileErrors.java
+++ CloudCoderDataAnalysis/src/org/cloudcoder/progsnap2/GatherCompileErrors.java
@@ -9,6 +9,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +23,7 @@ import org.cloudcoder.app.server.submitsvc.IFutureSubmissionResult;
 import org.cloudcoder.app.server.submitsvc.oop.OutOfProcessSubmitService;
 import org.cloudcoder.app.shared.model.CompilationOutcome;
 import org.cloudcoder.app.shared.model.CompilationResult;
+import org.cloudcoder.app.shared.model.CompilerDiagnostic;
 import org.cloudcoder.app.shared.model.Language;
 import org.cloudcoder.app.shared.model.Problem;
 import org.cloudcoder.app.shared.model.ProblemAndTestCaseList;
@@ -40,6 +43,8 @@ public class GatherCompileErrors {
 
 	//private Map<Integer, Problem> problemMap;
 	private Map<Integer, ProblemAndTestCaseList> problemMap;
+
+	private File diagDir;
 	
 	public GatherCompileErrors() {
 		problemMap = new HashMap<Integer, ProblemAndTestCaseList>();
@@ -78,8 +83,6 @@ public class GatherCompileErrors {
 		
 		Scanner keyboard = new Scanner(System.in);
 		Properties config = Export.getExportConfig(args, keyboard);
-		
-		Export.askIfMissing(config, "ps2.gccExe", "gcc executable name: ", keyboard);
 
 		Util.connectToDatabase(config);
 		
@@ -89,69 +92,115 @@ public class GatherCompileErrors {
 		if (updatedMainEventTable.exists()) {
 			throw new IllegalStateException(updatedMainEventTable.getAbsolutePath() + " already exists!");
 		}
-
-		try (FileOutputStream fos = new FileOutputStream(updatedMainEventTable)) {
-			OutputStreamWriter wr = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
-			CSVWriter w = new CSVWriter(wr);
+		
+		// Create a directory where compiler diagnostics can be written
+		this.diagDir = new File(baseDir, "diag");
+		diagDir.mkdirs();
+		
+		try (FileInputStream fis = new FileInputStream(baseDir + "/MainTable.csv")) {
+			InputStreamReader rd = new InputStreamReader(fis, StandardCharsets.UTF_8);
+			@SuppressWarnings("resource")
+			CSVReader r = new CSVReader(rd);
 			
-			try (FileInputStream fis = new FileInputStream(baseDir + "/MainTable.csv")) {
-				InputStreamReader rd = new InputStreamReader(fis, StandardCharsets.UTF_8);
-				@SuppressWarnings("resource")
-				CSVReader r = new CSVReader(rd);
+			// read header
+			String[] header = r.readNext();
+			
+			// TODO: header for output CSV (add column(s) for source location, error text) 
+			
+			for (;;) {
+				String[] row = r.readNext();
+				if (row == null) { break; }
+				Map<String, String> rowValues = new HashMap<String, String>();
+				for (int i = 0; i < row.length; i++) {
+					rowValues.put(header[i], row[i] != null ? row[i] : "");
+				}
 				
-				// read header
-				String[] header = r.readNext();
-				
-				// TODO: header for output CSV (add column(s) for source location, error text) 
-				
-				for (;;) {
-					String[] row = r.readNext();
-					if (row == null) { break; }
-					Map<String, String> rowValues = new HashMap<String, String>();
-					for (int i = 0; i < row.length; i++) {
-						rowValues.put(header[i], row[i] != null ? row[i] : "");
-					}
+				if (rowValues.get("EventType").equals("Compile")) {
+					int problemId = Integer.parseInt(rowValues.get("ProblemID"));
+					ProblemAndTestCaseList p = getProblem(problemId);
 					
-					if (rowValues.get("EventType").equals("Compile")) {
-						int problemId = Integer.parseInt(rowValues.get("ProblemID"));
-						ProblemAndTestCaseList p = getProblem(problemId);
+					if (p.getProblem().getProblemType().getLanguage() == Language.C) {
+						// Is a C problem!
 						
-						if (p.getProblem().getProblemType().getLanguage() == Language.C) {
-							// Is a C problem!
+						System.out.printf("Event %s is a C compilation\n", rowValues.get("EventID"));
+						
+						if (rowValues.get("ProgramResult").equals("Error")) {
+							// This submission failed to compile!
+							System.out.printf("  ==> Event %s is a failed C compilation\n", rowValues.get("EventID"));
 							
-							System.out.printf("Event %s is a C compilation\n", rowValues.get("EventID"));
+							// Get the code
+							String code;
+							String codeFileName = baseDir + "/CodeStates/" + rowValues.get("CodeStateSection");
+							System.out.printf("  Code file name is %s\n", codeFileName);
+							try (InputStream in = new FileInputStream(codeFileName)) {
+								code = IOUtils.toString(in);
+							}
 							
-							if (rowValues.get("ProgramResult").equals("Error")) {
-								// This submission failed to compile!
-								System.out.printf("  ==> Event %s is a failed C compilation\n", rowValues.get("EventID"));
+							// Submit the crud
+							try {
+								System.out.printf("  Recompiling event %s\n", rowValues.get("EventID"));
+								IFutureSubmissionResult future = svc.submitAsync(p.getProblem(), p.getTestCaseData(), code);
+								SubmissionResult result = null;
+								do {
+									result = future.waitFor(100L);
+								} while (result == null);
+								System.out.printf("  Successfully recompiled event %s\n", rowValues.get("EventID"));
 								
-								// Get the code
-								String code;
-								String codeFileName = baseDir + "/CodeStates/" + rowValues.get("CodeStateSection");
-								System.out.printf("  Code file name is %s\n", codeFileName);
-								try (InputStream in = new FileInputStream(codeFileName)) {
-									code = IOUtils.toString(in);
-								}
-								
-								// Submit the crud
-								try {
-									System.out.printf("  Recompiling event %s\n", rowValues.get("EventID"));
-									IFutureSubmissionResult future = svc.submitAsync(p.getProblem(), p.getTestCaseData(), code);
-									SubmissionResult result = null;
-									do {
-										result = future.waitFor(100L);
-									} while (result == null);
-									System.out.printf("  Successfully recompiled event %s\n", rowValues.get("EventID"));
-									
-									CompilationResult cr = result.getCompilationResult();
-									if (cr.getOutcome() != CompilationOutcome.FAILURE) {
-										logger.warn("Compilation outcome wasn't failure?");
+								CompilationResult cr = result.getCompilationResult();
+								if (cr.getOutcome() != CompilationOutcome.FAILURE) {
+									logger.warn("Compilation outcome wasn't failure?");
+								} else {
+									String eventId = rowValues.get("EventID");
+									File diagOut = new File(this.diagDir, eventId + ".csv");
+									if (diagOut.exists()) {
+										System.out.println("Diagnostic file for event " + eventId + " already exists");
 									} else {
-										//cr.getCompilerDiagnosticList()
+										File diagOutTmp = new File(this.diagDir, eventId + "-tmp.csv");
+										try (FileOutputStream os = new FileOutputStream(diagOutTmp)) {
+											OutputStreamWriter pw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+											CSVWriter dw = new CSVWriter(pw);
+											dw.writeNext("EventType", "ParentEventID", "CodeStateSection", "SourceLocation",
+													"CompileMessageType", "CompileMessageData");
+											
+											for (CompilerDiagnostic d : cr.getCompilerDiagnosticList()) {
+												String message = d.getMessage();
+												String eventType = message.startsWith("error:") ? "Compile.Error" : "Compile.Warning";
+												String compileMessageType = message.startsWith("error:") ? "Error" : "Warning";
+												StringBuilder loc = new StringBuilder();
+												loc.append("Text:");
+												loc.append(d.getStartLine());
+												if (d.getStartColumn() >= -1) {
+													// Column information reported by gcc is 0-based,
+													// convert to 1-based
+													loc.append(":");
+													loc.append(d.getStartColumn() + 1);
+												}
+												
+												dw.writeNext(
+														eventType,
+														eventId,
+														rowValues.get("CodeStateSection"),
+														loc.toString(),
+														compileMessageType,
+														message);
+											}
+											
+											dw.flush();
+										}
+										
+										// We seem to have written the data successfully, so commit it
+										// to the correct file name.
+										Files.move(diagOutTmp.toPath(), diagOut.toPath(),
+												StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
+										
+										System.out.printf("  Successfully recorded %d diagnostics for compile event %s\n ",
+												cr.getCompilerDiagnosticList().length, eventId);
 									}
-								} catch (SubmissionException e) {
-									logger.error("Exception sending submission for recompilation", e);
+									
+									//cr.getCompilerDiagnosticList()
 								}
+							} catch (SubmissionException e) {
+								logger.error("Exception sending submission for recompilation", e);
 							}
 						}
 					}
